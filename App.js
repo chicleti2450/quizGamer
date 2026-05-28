6664a6fa33a777a562bbf94dc08be30a242fded5
chicleti2450 <leticiadomingos2450@gmail.com>
2026-05-28 13:07:52 -0300

    quiz-28/05

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// useState é uma ferramenta que cria memória quando o valor muda, a tela 
+// atualiza automaticamente
+import React, {useState} from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// View -> é como uma caixa que organiza os elementos
+// Text -> é o único jeito de mostrar texto na tela
+// TouchableOpacity -> botão que podemos personalizar
+// StyleSheet -> onde criamos os estilos (css sem js)
+// Animated -> permite criar animações suaves
+// Easing -> controla o ritmo das animações
+import {View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Animated,
+  Easing,
+} from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+// Banco de perguntas 
+// uma lista de (array) de projetos
+// Cada objeto é uma pergunta com suas opções de respostas e a resposta correta.
+const PERGUNTAS = [
+  {
+    pergunta: 'Qual a empresa criou o React?',
+    opcoes:['Google', 'Meta', 'Microsoft', 'Apple'],
+    correta: 'Meta',
+    emoji: '⚛️',
   },
-});
+  
+  {
+    pergunta: 'Qual linguagem o React Native usa?',
+    opcoes:['Python', 'Java', 'JavaScript', 'PHP'],
+    correta: 'JavaScript',
+    emoji: '💛',
+  }, 
+
+  {
+    pergunta: 'Qual o hook usam para criar estado?',
+    opcoes:['Propos', 'Hooks', 'Estado(State)', 'Componentes'],
+    correta: 'Estado(State)',
+    emoji: '🎣',
+  }, 
+
+    {
+    pergunta: 'O que JSX significa?',
+    opcoes:['Java Style Extension', 'JavaScript XML', 
+      'JSON Syntax extra', 'Just Some example'],
+    correta: 'JavaScript XML',
+    emoji: '🏷️',
+  }, 
+
+    {
+    pergunta: 'O que é um componente no React?',
+    opcoes:['Um banco de dados', 'um pedaço reutilizável da tela',
+       'um servidor', 'um arquivo css'],
+    correta: 'um pedaço reutilizável da tela',
+    emoji: '🧠',
+  }, 
+
+    {
+    pergunta: 'Como chamamos as "caixinhas de memória" do React?',
+    opcoes:['', '', '', ''],
+    correta: '',
+    emoji: '🧩',
+  }, 
+
+    {
+    pergunta: '',
+    opcoes:['', '', '', ''],
+    correta: '',
+    emoji: '📦',
+  }, 
+
+    {
+    pergunta: '',
+    opcoes:['', '', '', ''],
+    correta: '',
+    emoji: '⚡',
+  }, 
+
+    {
+    pergunta: '',
+    opcoes:['', '', '', ''],
+    correta: '',
+    emoji: '🔄',
+  }, 
+
+    {
+    pergunta: '',
+    opcoes:['', '', '', ''],
+    correta: '',
+    emoji: '✍️',
+  }, 
+];
